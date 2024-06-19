0e67922734f433d964745b8dc574afa391363a80
Dylanrusere <dylaneight01@gmail.com>
2024-06-19 22:28:16 +0200

    done with the popular docs page

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// Hamburger Menu
+const hamMenu = document.querySelector(".ham_menu");
+const offMenu = document.querySelector(".offScreenMenu");
+
+hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+    offMenu.classList.toggle("active");
+});
+
+
+// Counter To 
+let targetNumber1 = 499;
+let targetNumber2 = 199;
+let counter1 = 0;
+let counter2 = 0;
+let counterElement1 = document.getElementById("counter_thousand");
+let counterElement2 = document.getElementById("counter_percentage");
+
+function incrementCounter1() {
+    counter1++;
+    counterElement1.textContent = counter1;
+
+    if (counter1 <= targetNumber1) {
+      requestAnimationFrame(incrementCounter1);
+    }
+  }
+
+  function incrementCounter2() {
+    counter2++;
+    counterElement2.textContent = counter2;
+
+    if (counter2 <= targetNumber2) {
+      requestAnimationFrame(incrementCounter2);
+    }
+  }
+
+incrementCounter1();
+incrementCounter2();
+
+// Search Bar
+const searchInput = document.getElementById('search_input');
+const searchButton = document.getElementById('search_icon');
+
+searchButton.addEventListener('click', () => {
+  const searchTerm = searchInput.value.trim();
+  if (searchTerm !== '') {
+    
+    console.log('Searching for:', searchTerm);
+  }
+});
+
+searchInput.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    searchButton.click();
+  }
+});
